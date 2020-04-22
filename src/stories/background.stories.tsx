6436ace7755b8f8b4d9560fcbb6237e29777587c
Unknown <mail.shuaibkhan@gmail.com>
2020-04-22 21:58:15 +0530

    Story Change

---
+++ src/stories/background.stories.tsx
@@ -0,0 +1,12 @@
+// import React, { FC } from 'react'
+
+// type BackgroundProps = {
+//     color: string,
+//     width: string
+// }
+
+// export const Background: FC<BackgroundProps> = ({color, width})=>(
+//     <div className="w-screen h-screen">
+
+//     </div>
+// )
--- src/stories/button.stories.tsx
+++ src/stories/button.stories.tsx
@@ -12,3 +12,14 @@ export const withEmoji = () => (
     </span>
   </button>
 );
+
+// type BackgroundProps = {
+//     color: string,
+//     width: string
+// }
+
+// export const Background: FC<BackgroundProps> = ({color, width})=>(
+//     <div className={`w-screen h-screen bg-purple`}>
+
+//     </div>
+// )
